b7abab71b45957f229e0373385c1316fbf41da5c
jowens-ss <153014271+jowens-ss@users.noreply.github.com>
2024-06-21 15:28:15 -0400

    Delete analysis/system/industrial_fleet directory

--- analysis/system/industrial_fleet/test_steel_fleet.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    from analysis.system.industrial_fleet.iron_steel_projections import SteelProjection
-    test = SteelProjection()
-    results = test.run()
-    test.plot(results)
